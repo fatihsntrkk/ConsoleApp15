5606ef7159017d25b71c5ddf6ccae97baca09baa
GAZİ TUSAS MYO <gazi.kazan.myo@outlook.com>
2019-12-11 11:39:21 +0300

    Add project files.

---
+++ ConsoleApp15/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayac = 0;
+            while (sayac<5)
+            Console.WriteLine("123");
+            sayac++;
+            
+
+        }
+    }
+}
